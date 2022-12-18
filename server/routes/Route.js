83134b7beb1cec8d3e0f5c7cdc9d638cec52dcd8
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-12-18 21:01:52 +0530

    done

--- server/routes/Route.js
+++ server/routes/Route.js
@@ -1,10 +1,6 @@
 import express from 'express';
 import { AddUser, getUser, } from '../controller/UserController.js';
 const router = express.Router();
-
 router.post("/add", AddUser)
 router.get("/all", getUser)
-
-
-
 export default router
